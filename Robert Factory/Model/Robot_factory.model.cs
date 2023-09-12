0e30255856f8a2f95f67533122554e8724ae4562
M4Z3D <marcuswind123@gmail.com>
2023-09-12 19:38:04 +0200

    Add project files.

---
+++ Robert Factory/Model/Robot_factory.model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Robot_Factory.Model
+{
+    public class Robot
+    {
+        public string task = "";
+        public string size = "";
+        public byte batteryCapacity;
+        public Color color;
+        public string chip = "";
+        public string wheel = "";
+
+        public Robot(string task, string size,byte batterCapacity, Color color, string chip, string wheel) 
+        
+        { 
+            this.task = task;
+            this.size = size;   
+            this.batteryCapacity = batterCapacity;  
+            this.color = color;
+            this.chip = chip;
+            this.wheel = wheel;
+        }
+        
+    }
+
+}
---
+++ Robert Factory/Program.cs
@@ -0,0 +1,49 @@
+﻿using Robot_Factory.Model;
+using System.Drawing;
+
+namespace Robert_Factory
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Robot robot1 = new Robot("Sweeper", " Small", 255, Color.White, " Default", " Has wheels");
+            Robot robot2 = new Robot("window Cleaner", " Big", 255, Color.Blue, " RX54667", " No Wheels");
+            Robot robot3 = new Robot("Tire Changer", " Big", 255, Color.Green, " QT8339", " No Wheels");
+
+            WriteToGUI(robot1, robot2, robot3);
+        }
+        private static void WriteToGUI(Robot robot1, Robot robot2, Robot robot3)
+        {
+            Console.WriteLine("robot 1");
+            Console.WriteLine($"task:{robot1.task}");
+            Console.WriteLine($"size:{robot1.size}");
+            Console.WriteLine($"Battery:{robot1.batteryCapacity}");
+            Console.WriteLine($"Color:{robot1.color}");
+            Console.WriteLine($"Chip:{robot1.chip}");
+            Console.WriteLine($"wheels:{robot1.wheel}");
+            Console.WriteLine();
+
+            Console.WriteLine("robot 2");
+            Console.WriteLine($"task:{robot2.task}");
+            Console.WriteLine($"size:{robot2.size}");
+            Console.WriteLine($"Battery:{robot2.batteryCapacity}");
+            Console.WriteLine($"Color:{robot2.color}");
+            Console.WriteLine($"Chip:{robot2.chip}");
+            Console.WriteLine($"wheels:{robot2.wheel}");
+            Console.WriteLine();
+
+            Console.WriteLine("robot 3");
+            Console.WriteLine($"task:{robot3.task}");
+            Console.WriteLine($"size:{robot3.size}");
+            Console.WriteLine($"Battery:{robot3.batteryCapacity}");
+            Console.WriteLine($"Color:{robot3.color}");
+            Console.WriteLine($"Chip:{robot3.chip}");
+            Console.WriteLine($"wheels:{robot3.wheel}");
+            Console.ReadLine();
+        }
+    }
+}
+ 
